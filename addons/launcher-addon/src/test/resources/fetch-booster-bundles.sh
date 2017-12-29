a5e8f35db7510ac8896a5481367c996371d5f434
Dipak Pawar <dpawar@redhat.com>
2017-12-29 17:21:06 +0530

    Moves all test resources from web to launcher-addon

--- addons/launcher-addon/src/test/resources/fetch-booster-bundles.sh
+++ addons/launcher-addon/src/test/resources/fetch-booster-bundles.sh
@@ -13,6 +13,17 @@ done
 git bundle create booster-catalog.bundle --all
 cp booster-catalog.bundle ${TEST_DIR}
 
+for repo in $(cat $(find . -type f -name '*.yaml') | grep githubRepo | cut -f2 -d' '); do
+    cd ${BOOSTER_TMP_DIR}
+    mkdir -p repos
+    cd repos
+    git clone https://github.com/$repo
+    REPO_NAME=$(echo $repo | cut -f2 -d'/')
+    cd $REPO_NAME
+    git bundle create ${REPO_NAME}.bundle --all
+    cp ${REPO_NAME}.bundle ${TEST_DIR}
+done
+
 cd ${TEST_DIR}
 
 rm -rf ${BOOSTER_TMP_DIR}
